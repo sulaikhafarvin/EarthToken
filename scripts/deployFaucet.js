a2428b0a35c51e18025c5513d9c7e57622620b63
Sulaikha Farvin <117733825+sulaikhafarvin@users.noreply.github.com>
2023-06-24 21:12:46 +0530

    Update deployFaucet.js

--- scripts/deployFaucet.js
+++ scripts/deployFaucet.js
@@ -1,19 +1,38 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
-  const Faucet = await ethers.getContractFactory("Faucet");
-  const faucet = await Faucet.deploy(
-    "0x309D0dD0010b628465850cAC060b34F8F37884EB"
-  );
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        const Faucet = await ethers.getContractFactory("Faucet")
+        // EarthToken address : 0x3c933fb202f6170eda0f1145751352aAE0024C30
+        const faucet = await Faucet.deploy("0x3c933fb202f6170eda0f1145751352aAE0024C30") 
+        await faucet.waitForDeployment()
+        const address = await faucet.getAddress()
+        console.log(`Earth Token deployed:${address}`)
 
-  await faucet.waitForDeployment();
-
-  const address = await faucet.getAddress();
-
-  console.log(`Faucet contract deployed:${address}`);
+        const args = ["0x3c933fb202f6170eda0f1145751352aAE0024C30"]
+        console.log("Waiting for block confirmations....")
+        await faucet.deploymentTransaction().wait(6)
+        await verify(address, args)
+    }
+}
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.error(error)
+    process.exitCode = 1
+})
+
